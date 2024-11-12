d41d721f3670f363f263d3b0b60e59ab658e85dd
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-11-12 12:04:39 +0530

    Delete LAB 2  02.01.2024 directory

--- LAB 2  02.01.2024/Info.java
+++
@@ -1,9 +0,0 @@
-public class Info
-{
-    public static void main(String[]args){
-        System.out.println("Name: " +args[0]);
-        System.out.println("Roll no: " +args[1]);
-        System.out.println("Branch: " +args[2]);
-        System.out.println("Semester: " +args[4]);
-    }
-}
--- LAB 2  02.01.2024/typecast.java
+++
@@ -1,9 +0,0 @@
-public class typecast {
-    
-    public static void main(String []args){
-    System.out.println(args[0]+args[1]);  //string concate thay che aaya
-    double num1 = Double.parseDouble(args[0]);
-    int num2 = Integer.parseInt(args[1]);
-    System.out.println(num1+num2);
-    }
-}
