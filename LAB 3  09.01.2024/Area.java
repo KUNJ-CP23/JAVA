199ca61795d9565605cc7ecc8f208f9b2bb56891
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-11-12 12:04:56 +0530

    Delete LAB 3  09.01.2024 directory

--- LAB 3  09.01.2024/Area.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Area {
-    public static void main(String []args){
-        Scanner sc = new Scanner (System.in);
-        System.out.print("Enter radius of circle = ");
-        int r = sc.nextInt();
-        System.out.print("Area = "+((double)r*r*Math.PI));
-    }
-}
