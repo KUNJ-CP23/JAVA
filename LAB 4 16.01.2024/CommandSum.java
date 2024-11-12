6438db0529aca2898d5db89f6b287a936779530e
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-11-12 12:05:11 +0530

    Delete LAB 4 16.01.2024 directory

--- LAB 4 16.01.2024/CommandSum.java
+++
@@ -1,9 +0,0 @@
-
-    public class CommandSum{
-        public static void main(String[] args){
-        double num1 = Double.parseDouble(args[0]);
-        int num2 = Integer.parseInt(args[1]);
-        System.out.println(num1+num2);
- 
-    }
-}
